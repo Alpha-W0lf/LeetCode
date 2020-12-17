5b5bb7f0d33fc73109702f74aa2bf5e572e4356b
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-17 08:41:52 -0600

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -3,27 +3,22 @@
 #
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 #
-# You can return the answer in any order.
-#
-#
+# You can return the answer in any order. DONE
 #
 # Example 1:
-#
 # Input: nums = [2,7,11,15], target = 9
 # Output: [0,1]
 # Output: Because nums[0] + nums[1] == 9, we return [0, 1].
-# Example 2:
 #
+# Example 2:
 # Input: nums = [3,2,4], target = 6
 # Output: [1,2]
-# Example 3:
 #
+# Example 3:
 # Input: nums = [3,3], target = 6
 # Output: [0,1]
 #
-#
 # Constraints:
-#
 # 2 <= nums.length <= 103
 # -109 <= nums[i] <= 109
 # -109 <= target <= 109
@@ -37,11 +32,11 @@ def twoSum(nums, target):
     for i in nums:
         remainder = target - i
         for y in nums:
-            if y != i and y == remainder:
+            if y > i and y == remainder:
                 print('[', i, ', ', y, ']', sep='')
                 break
 
-# TODO: Figure out how to break after getting first working pair.
+# Figure out how to break after getting first working pair. DONE
 
 nums_in = [2, 7, 11, 15]
 target_in = 18
